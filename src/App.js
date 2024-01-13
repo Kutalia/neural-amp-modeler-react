a3f96b24aed13cbc167b29072fb88454913aa875
Kote Kutalia <kotekutalia@gmail.com>
2024-01-13 22:02:53 +0400

    code refactoring and cabinet ir swapping support added

--- src/App.js
+++ src/App.js
@@ -4,88 +4,89 @@ import { getCabConvolver } from './helpers/getCabConvolver';
 import './App.css';
 import { readProfile } from './helpers/readProfile';
 
-let audioContext, audioWorkletNode;
-
-window.wasmAudioWorkletCreated = (node1, node2) => {
-  audioWorkletNode = node1;
-  audioContext = node2;
-
-  const resumerBtn = document.getElementById('audio-worklet-resumer')
-
-  if (resumerBtn) {
-    resumerBtn.disabled = false;
-  }
-};
-
 function App() {
   const diAudioRef = useRef();
   const [useDi, setUseDi] = useState(false);
-  const [irReady, setIrReady] = useState(false);
-  const [dspRunning, setDspRunning] = useState(false);
-  const [audioResumed, setAudioResumed] = useState(false);
+  const [ir, setIr] = useState();
+  const [audioContext, setAudioContext] = useState();
+  const [audioWorkletNode, setAudioWorkletNode] = useState();
+  const microphoneStreamNodeRef = useRef();
+  const diTrackStreamSource = useRef();
 
-  const onCabReady = async (cabConvolver) => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        autoGainControl: false,
-        echoCancellation: false,
-        noiseSuppression: false,
-      }
-    });
-
+  const onCabChange = (cabConvolver) => {
     audioContext.resume();
 
-    if (useDi) {
-      const audioElement = diAudioRef.current;
-      const diTrackStreamSource = audioContext.createMediaElementSource(audioElement);
-      diTrackStreamSource.connect(audioWorkletNode);
-    } else {
-      const microphoneStreamNode = audioContext.createMediaStreamSource(stream);
-      microphoneStreamNode.connect(audioWorkletNode);
+    // disconnect old impulse response
+    if (ir) {
+      audioWorkletNode.disconnect();
+      ir.disconnect();
     }
 
     audioWorkletNode.connect(cabConvolver);
     cabConvolver.connect(audioContext.destination);
-    setIrReady(true);
+    setIr(cabConvolver);
   };
 
   const onIRInput = (event) => {
     if (audioContext && event.target.files?.length) {
-      event.target.files[0].arrayBuffer().then(buffer => getCabConvolver(audioContext, buffer, onCabReady));
+      event.target.files[0].arrayBuffer().then(buffer => getCabConvolver(audioContext, buffer, onCabChange));
     }
   };
 
-  const loadProfile = (profile) => {
-    window.setDsp = (jsonStr) => {
-      const { Module } = window
-      const ptr = Module._malloc(jsonStr.length + 1);
-      Module.stringToUTF8(jsonStr, ptr, jsonStr.length + 1)
+  const loadProfile = (jsonStr) => {
+    const { Module } = window
+    const ptr = Module._malloc(jsonStr.length + 1);
+    Module.stringToUTF8(jsonStr, ptr, jsonStr.length + 1)
 
-      if (audioContext) {
-        audioContext.suspend();
+    if (audioContext) {
+      audioContext.suspend();
+    }
+
+    window.wasmAudioWorkletCreated = (node1, node2) => {
+      setAudioWorkletNode(node1);
+      setAudioContext(node2);
+
+      // initializing various web audio nodes when user is interacting for the first time
+
+      if (!microphoneStreamNodeRef.current) {
+        const audioWorkletNode = node1;
+        const audioContext = node2;
+
+        navigator.mediaDevices.getUserMedia({
+          audio: {
+            autoGainControl: false,
+            echoCancellation: false,
+            noiseSuppression: false,
+          }
+        }).then((microphoneStream) => {
+          if (useDi && !diTrackStreamSource.current) {
+            const audioElement = diAudioRef.current;
+            diTrackStreamSource.current = audioContext.createMediaElementSource(audioElement);
+            diTrackStreamSource.current.connect(audioWorkletNode);
+          } else if (!microphoneStreamNodeRef.current) {
+            microphoneStreamNodeRef.current = audioContext.createMediaStreamSource(microphoneStream);
+            microphoneStreamNodeRef.current.connect(audioWorkletNode);
+          }
+        }).catch((err) => {
+          console.log('Please allow your guitar input from the browser');
+        });
       }
-
-      Module.ccall(
-        "setDsp",
-        null,
-        ["number"],
-        [ptr],
-        {
-          async: true,
-        }
-      ).then(() => {
-        Module._free(ptr);
-        if (audioContext) {
-          audioContext.resume();
-        }
-      });
     };
-    window.setDsp(profile);
-    setDspRunning(true);
-  };
 
-  const onResume = () => {
-    setAudioResumed(true);
+    Module.ccall(
+      "setDsp",
+      null,
+      ["number"],
+      [ptr],
+      {
+        async: true,
+      }
+    ).then(() => {
+      Module._free(ptr);
+      if (audioContext) {
+        audioContext.resume();
+      }
+    });
   };
 
   const onProfileInput = (event) => {
@@ -96,14 +97,15 @@ function App() {
 
   return (
     <div className="App">
-      <button id="audio-worklet-resumer" onClick={onResume} disabled={!!window.audioWorkletNode}>Start playing</button>
+      {/* Just another way to resume audioContext from wasm glue code */}
+      <button id="audio-worklet-resumer" disabled={window.audioWorkletNode}>Start/Resume playing</button>
       <div>
         <label htmlFor="profile">Choose NAM profile</label>
-        <input type="file" id="profile" accept=".nam" onChange={onProfileInput} disabled={!audioResumed} />
+        <input type="file" id="profile" accept=".nam" onChange={onProfileInput} />
       </div>
       <div>
         <label htmlFor="ir">Choose ir</label>
-        <input type="file" id="ir" accept="audio/*" onChange={onIRInput} disabled={!dspRunning || irReady} />
+        <input type="file" id="ir" accept="audio/*" onChange={onIRInput} disabled={!audioContext} />
         <p>Please reload page to change IR</p>
       </div>
       {
