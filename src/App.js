9ee9fb5a8b87c3d85dcaa0d3db06fcd90a6aa212
Kote Kutalia <kotekutalia@gmail.com>
2024-01-13 22:24:29 +0400

    quick switching support for microphone and di track inputs added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import { getCabConvolver } from './helpers/getCabConvolver';
 import './App.css';
@@ -6,7 +6,7 @@ import { readProfile } from './helpers/readProfile';
 
 function App() {
   const diAudioRef = useRef();
-  const [useDi, setUseDi] = useState(false);
+  const [useDiTrack, setUseDiTrack] = useState(null);
   const [ir, setIr] = useState();
   const [audioContext, setAudioContext] = useState();
   const [audioWorkletNode, setAudioWorkletNode] = useState();
@@ -59,16 +59,19 @@ function App() {
             noiseSuppression: false,
           }
         }).then((microphoneStream) => {
-          if (useDi && !diTrackStreamSource.current) {
-            const audioElement = diAudioRef.current;
-            diTrackStreamSource.current = audioContext.createMediaElementSource(audioElement);
+          // preparing mic and di track nodes for quicker future switching
+          const audioElement = diAudioRef.current;
+          diTrackStreamSource.current = audioContext.createMediaElementSource(audioElement);
+
+          microphoneStreamNodeRef.current = audioContext.createMediaStreamSource(microphoneStream);
+
+          if (useDiTrack) {
             diTrackStreamSource.current.connect(audioWorkletNode);
-          } else if (!microphoneStreamNodeRef.current) {
-            microphoneStreamNodeRef.current = audioContext.createMediaStreamSource(microphoneStream);
+          } else {
             microphoneStreamNodeRef.current.connect(audioWorkletNode);
           }
         }).catch((err) => {
-          console.log('Please allow your guitar input from the browser');
+          console.log('Error occured during input connecting', err);
         });
       }
     };
@@ -89,16 +92,41 @@ function App() {
     });
   };
 
+  useEffect(() => {
+    // if input mode is changed manually from the DOM
+    if (useDiTrack !== null && microphoneStreamNodeRef.current && diTrackStreamSource.current && audioWorkletNode) {
+      if (useDiTrack) {
+        microphoneStreamNodeRef.current.disconnect();
+        diTrackStreamSource.current.connect(audioWorkletNode);
+      } else {
+        diTrackStreamSource.current.disconnect();
+        microphoneStreamNodeRef.current.connect(audioWorkletNode);
+      }
+    }
+  }, [useDiTrack, audioWorkletNode]);
+
   const onProfileInput = (event) => {
     readProfile(event).then((profile) => {
       loadProfile(profile);
     });
   };
 
+  const onInputModeChange = (e) => {
+    if (e.target.checked) {
+      setUseDiTrack(true);
+    } else {
+      setUseDiTrack(false);
+    }
+  };
+
   return (
     <div className="App">
       {/* Just another way to resume audioContext from wasm glue code */}
       <button id="audio-worklet-resumer" disabled={window.audioWorkletNode}>Start/Resume playing</button>
+      <div>
+        <label htmlFor="input-mode">Use DI track for testing (bypasses microphone)</label>
+        <input type="checkbox" id="input-mode" onChange={onInputModeChange} />
+      </div>
       <div>
         <label htmlFor="profile">Choose NAM profile</label>
         <input type="file" id="profile" accept=".nam" onChange={onProfileInput} />
@@ -108,11 +136,9 @@ function App() {
         <input type="file" id="ir" accept="audio/*" onChange={onIRInput} disabled={!audioContext} />
         <p>Please reload page to change IR</p>
       </div>
-      {
-        useDi &&
-        <audio controls ref={diAudioRef}>
-          <source src={`${process.env.PUBLIC_URL}/LasseMagoDI.mp3`} type="audio/mpeg" />
-        </audio>
+      <audio controls ref={diAudioRef}>
+        <source src={`${process.env.PUBLIC_URL}/LasseMagoDI.mp3`} type="audio/mpeg" />
+      </audio>
       }
     </div>
   );
