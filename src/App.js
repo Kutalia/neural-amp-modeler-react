83e4ce46189987af3478cba8564cdbe886b45ad9
Kote Kutalia <kotekutalia@gmail.com>
2024-01-18 22:00:03 +0400

    vercel speedinsights correction for react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import * as stylex from '@stylexjs/stylex';
 
 import { getCabConvolver } from './helpers/getCabConvolver';
