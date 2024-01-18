b0168043028bab01f69d3c067dbf0648c6f327dc
Kote Kutalia <kotekutalia@gmail.com>
2024-01-18 21:51:01 +0400

    vercel speed insights integrated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import * as stylex from '@stylexjs/stylex';
 
 import { getCabConvolver } from './helpers/getCabConvolver';
@@ -190,6 +191,7 @@ function App() {
       </div>
 
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
